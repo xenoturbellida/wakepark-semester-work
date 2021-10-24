77041242baa417c9138d2d23545002db9169c4af
xenoturbellida <ElPGordeeva@stud.kpfu.ru>
2021-10-24 13:20:18 +0300

    introduce flask-migrate with great effort

--- wakepark/models.py
+++ wakepark/models.py
@@ -13,20 +13,3 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(40), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     authenticated = db.Column(db.Boolean, default=False)
-
-    # def is_active(self):
-    #     """True, as all users are active."""
-    #     return True
-    #
-    # def get_id(self):
-    #     """Return the email address to satify Flask-Login's requirements."""
-    #     return self.email
-    #
-    # def is_authenticated(self):
-    #     """Return True if the user is authenticated."""
-    #     return self.authenticated
-    #
-    # def is_anonymous(self):
-    #     """False, as anonymous users aren't supported."""
-    #     return False
-
--- wakepark/park.py
+++ wakepark/park.py
@@ -1,67 +1,58 @@
-import os
-
-from flask import Flask, request, render_template, Blueprint, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user
 from flask_migrate import Migrate
-
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
-import models
 from wakepark.cashier.cashier import cashier
-
+from wakepark.forms import RegisterForm, LoginForm
+from wakepark.models import db, User
 
 DATABASE = 'database.db'
 DEBUG = True
 SECRET_KEY = 'thisisasecretkey'
 
-park = Flask(__name__)
-park.config.from_object(__name__)
-park.config.update(dict(DATABASE=os.path.join(park.root_path, 'database.db')))
-park.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-park.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-models.db.init_app(park)
-migrate = Migrate(park, models.db)
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.register_blueprint(cashier, url_prefix='/cashier')
+db.init_app(app)
 
-import forms
-
-park.register_blueprint(cashier, url_prefix='/cashier')
+migrate = Migrate(app, db)
 
 login_manager = LoginManager()
-login_manager.init_app(park)
+login_manager.init_app(app)
 login_manager.login_view = "login"
-
-
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    return models.User.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
 
-@park.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
 
-@park.route('/signup', methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = forms.RegisterForm()
+    form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data)
-        new_user = models.User(email=form.email.data, password=hashed_password)
+        new_user = User(email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
     return render_template('signup.html', form=form)
 
 
-@park.route('/login', methods=['POST', 'GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    form = forms.LoginForm()
+    form = LoginForm()
     if form.validate_on_submit():
-        user = models.User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user:
             if check_password_hash(user.password, form.password.data):
                 remember = bool(form.remember_me.data)
@@ -70,12 +61,17 @@ def login():
     return render_template('login.html', form=form)
 
 
-@park.route('/logout')
+@app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("login"))
 
 
+@app.route("/hello")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
 if __name__ == '__main__':
-    park.run(debug=True)
+    app.run(debug=True)
