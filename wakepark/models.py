4af8e1c18684d86fe2d4bc38c2baa802d4fcf1c0
xenoturbellida <ElPGordeeva@stud.kpfu.ru>
2021-11-04 17:18:53 +0300

    create photos and posts tables

--- wakepark/models.py
+++ wakepark/models.py
@@ -1,6 +1,7 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -12,3 +13,22 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(40), unique=True, nullable=False)
     password = db.Column(db.String(105), nullable=False)
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text)
+    pubdate = db.Column(db.Date, nullable=False)
+    photos = relationship("Photo", back_populates='post')
+
+
+class Photo(db.Model):
+    __tablename__ = 'photos'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    path = db.Column(db.String(256))
+    post_id = db.Column(db.Integer, ForeignKey('posts.id'))
+    post = relationship('Post', back_populates='photos')
